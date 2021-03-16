f4e30287993aef97384769da20e87a75cd3f7601
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-03-17 02:06:40 +0900

    list

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("리스트 입니다!"),
         ),
+        drawer: Drawer(
+          child: MyDrawer(),
+        ),
         body: Center(
           child: _buildList(),
         ),
@@ -26,6 +29,77 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+// 상태를 가지는 StatefulWidget
+class MyDrawer extends StatefulWidget{
+  @override
+  _MyDrawer createState() {
+    print("MyDrawer - createState() called");
+    return _MyDrawer();
+  }
+}
+
+// 상태
+class _MyDrawer extends State<MyDrawer> {
+  int _myColorIndex = 0;
+  final colors = [Colors.orange, Colors.pink, Colors.lightGreenAccent, Colors.indigoAccent];
+
+  void _updateColorIndex(int updatedValue){
+    print("_MyDrawer - _updateColorIndex() called / updatedValue: $updatedValue");
+    setState(() {
+      _myColorIndex = updatedValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("MyDrawer - build() called");
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(child: Text("헤더 입니다!!"),
+            decoration: BoxDecoration(
+                color: colors[_myColorIndex]
+            ),
+          ),
+          ListTile(
+            title: Text("프로필"),
+            onTap: (){
+              print("프로필이 선택되었다");
+              _updateColorIndex(1);
+            },
+          ),
+          ListTile(
+            title: Text("설정"),
+            onTap: (){
+              print("설정이 선택되었다");
+              _updateColorIndex(2);
+            },
+          ),
+          ListTile(
+            title: Text("로그아웃"),
+            onTap: (){
+              print("로그아웃이 선택되었다");
+              _updateColorIndex(3);
+            },
+          ),
+          ListTile(
+            title: Text("닫기"),
+            onTap: (){
+              print("닫기가 선택되었다");
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+}
+
 
 Widget _buildList() => ListView(
   children: [
